9d3b907b34b0a5e963dfa838624c719d008e4e38
Tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2019-12-30 14:51:51 +0000

    showのルーティング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    user_path(resource.id) # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#home'
-  get "home/about" => "home#about"
+  get "home/about"
+
+  resources :users, only: [:index, :edit, :show, :destroy]
+  resources :books, only: [:index, :edit, :show, :destroy]
+
 end
