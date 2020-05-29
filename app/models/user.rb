109ce148f4f2917f2e2a7a00c1f95b56ecf8166c
TTTTk84 <bjjt1290058@gn.iwasaki.ac.jp>
2020-05-29 19:32:23 +0900

    [Add] フォロー機能関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
   validates :email, presence: true
   validates :name, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
